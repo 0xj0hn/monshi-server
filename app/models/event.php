5c7c3341e9d7dc014a622d0b06877d31f0c29f3d
John Peterson <nxh7@duck.com>
2023-10-07 23:07:08 +0330

    feat: add "get", "add", "remove" fn for events

---
+++ app/models/event.php
@@ -0,0 +1,36 @@
+<?php
+class Event extends Model {
+    public function addEvent() {
+
+    }
+
+    public function removeEvent() {
+
+    }
+
+    public function getEvents($managerId, $secretaryId) {
+        $sql = "SELECT * FROM events WHERE manager_id = ? AND secretary_id = ?";
+        $query = $this->query($sql, "ii", $managerId, $secretaryId);
+        $events = [];
+        $result = $query->get_result();
+        if ($result->num_rows > 0) {
+            while($row = $result->fetch_assoc()) {
+                $events[] = $row;
+            }
+            return $events;
+        }
+        return false;
+    }
+
+    public function getEvent($eventId, $managerId, $secretaryId) {
+        $sql = "SELECT * FROM events WHERE id = ? AND manager_id = ? AND secretary_id = ?";
+        $query = $this->query($sql, "iii", $eventId, $managerId, $secretaryId);
+        $result = $query->get_result();
+        if ($result->num_rows > 0) {
+            while($row = $result->fetch_assoc()) {
+                return $row;
+            }
+        }
+        return false;
+    }
+}
