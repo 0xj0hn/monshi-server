e017611e7e41a5c6008a83383d4e3ccff46a0ac0
John Peterson <nxh7@duck.com>
2023-10-13 15:22:36 +0330

    feat: implement JWT authentication

--- app/models/auth.php
+++ app/models/auth.php
@@ -1,5 +1,39 @@
 <?php
 class AuthModel extends Model {
+    private $secretKey = "MoNSHISeCrEt@^%!&^$";
+    public function setSecretKey($newSecretKey) {
+        $this->secretKey = $newSecretKey;
+    }
+
+    public function generateJWTToken($username, $seconds=0) {
+        $header = [
+            "typ" => "JWT",
+            "alg" => "HS256"
+        ];
+        $payload = [
+            "usr" => $username,
+            "iss" => "monshi",
+            "sub" => "auth",
+            "iat" => time()
+        ];
+        if ($seconds > 0) {
+            $payload["exp"] = time() + $seconds;
+        }
+        $encodedHeader = $this->base64UrlEncode(json_encode($header));
+        $encodedPayload = $this->base64UrlEncode(json_encode($payload));
+        $signature = hash_hmac('sha256', $encodedHeader . '.' . $encodedPayload, $this->secretKey, true);
+        $encodedSignature = $this->base64UrlEncode($signature);
+        $jwt = $encodedHeader . '.' . $encodedPayload . '.' . $encodedSignature;
+        return $jwt;
+    }
+
+    private function base64UrlEncode($value) {
+        $base64Encoded = base64_encode($value);
+        $base64UrlEncoded = strtr($base64Encoded, '+/', '-_');
+        $trimmedBase64UrlEncoded = rtrim($base64UrlEncoded, '=');
+        return $trimmedBase64UrlEncoded;
+    }
+
     //The below two functions are duplicated. Should be fixed later.
     public function secretaryLogin($username, $password) {
         $sql = "SELECT username, password FROM secretaries WHERE username = ? AND password = ?";
